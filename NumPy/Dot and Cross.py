cd4cd1c074625983cd066fde49e9390fe3932754
master <mavrovski@gmail.com>
2020-01-23 13:33:45 +0200

    DotAndCross

---
+++ NumPy/Dot and Cross.py
@@ -0,0 +1,9 @@
+import numpy
+
+
+import pdb;pdb.set_trace()
+n = int(input())
+a = numpy.array([input().split() for _ in range(n)],int)
+b = numpy.array([input().split() for _ in range(n)],int)
+
+print(numpy.dot(a,b))
